534c554eefa5622a1f5a8f5a30ee977494e763f9
Clown <1104480806@qq.com>
2020-07-07 23:28:18 +0800

    创建 icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="nav">
-    <router-link to="/money">Money</router-link> |
-    <router-link to="/labels">Label</router-link> |
-    <router-link to="/statistics">Statistics</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      Money
+    </router-link>
+    |
+    <router-link to="/labels">
+      <Icon name="label"/>
+      Label</router-link>
+    |
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      Statistics</router-link>
   </div>
 </template>
 
 <script lang="ts">
+  // import x from '@/assets/icons/money.svg';
   export default {
     name: 'Nav'
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
+  .nav {
     border: 1px solid red;
   }
 </style>
